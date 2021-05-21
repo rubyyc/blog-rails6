9275f474942fe9844eda1f0504d4eb103e88d630
YuanChang <759337828@qq.com>
2021-05-21 23:16:38 +0800

    set :default_env

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,11 @@ set :deploy_to, '/home/deploy/blog-rails6'
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+set :default_env, {
+  PATH: '$HOME/.npm-packages/bin/:$PATH',
+  NODE_ENVIRONMENT: 'production'
+}
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
