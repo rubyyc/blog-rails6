34588517a285a09e0f1141aa916f0b6f1986cc5f
YuanChang <759337828@qq.com>
2021-05-21 22:53:25 +0800

    fix: You can limit the size of your bundles by using import()

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig({
+module.exports = environment.toWebpackConfig().merge({
     performance: {
         hints: 'error',
         maxAssetSize: 300000, // 整数类型（以字节为单位）
