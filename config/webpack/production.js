5f68c3c13f80721b3ce7f816f68b6e9a651e1827
YuanChang <759337828@qq.com>
2021-05-24 09:53:29 +0800

    ERROR in asset size limit: The following asset(s) exceed the recommended size limit (293 KiB).

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,7 +5,7 @@ const environment = require('./environment')
 module.exports = environment.toWebpackConfig().merge({
     performance: {
         hints: 'error',
-        maxAssetSize: 300000, // 整数类型（以字节为单位）
-        maxEntrypointSize: 500000 // 整数类型（以字节为单位）
+        maxAssetSize: 3000000000, // 整数类型（以字节为单位）
+        maxEntrypointSize: 50000000 // 整数类型（以字节为单位）
     }
 })
